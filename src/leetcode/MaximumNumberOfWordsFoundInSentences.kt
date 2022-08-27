25b00ab5a4b3dea403d9c07ff386ef31238de5e7
Piri <mohammadfazelp@gmail.com>
2022-08-27 08:58:47 +0430

    Maximum Number Of Words Found In Sentences

---
+++ src/leetcode/MaximumNumberOfWordsFoundInSentences.kt
@@ -0,0 +1,27 @@
+package leetcode
+// Question link : https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/submissions/
+import java.util.regex.Pattern
+
+fun main() {
+    val m = MaximumNumberOfWordsFoundInSentences()
+    val result1 = m.mostWordsFound(arrayOf("alice and bob love leetcode", "i think so too", "this is great thanks very much"))
+    val result2 = m.mostWordsFound(arrayOf("please wait", "continue to fight", "continue to win"))
+    println("result : $result1")
+    println("result : $result2")
+}
+
+class MaximumNumberOfWordsFoundInSentences {
+
+    fun mostWordsFound(sentences: Array<String>): Int {
+        val pattern = Pattern.compile("\\s+")
+        var max = pattern.split(sentences[0].trim()).count()
+        sentences.forEach {
+            val words = pattern.split(it.trim())
+            if (words.count() > max) {
+                max = words.count()
+            }
+        }
+
+        return max
+    }
+}
