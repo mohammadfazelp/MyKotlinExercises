7003b653274f979f04653ec69b6a03dce19ed587
Piri <mohammadfazelp@gmail.com>
2022-08-30 18:33:22 +0430

    Sealed class example

--- src/topics/classes/sealed/MySealedClass.kt
+++ src/topics/classes/sealed/MySealedClass.kt
@@ -15,17 +15,34 @@ package topics.classes.sealed
  * instances, each with its own state.
  */
 
-import java.io.File
-import javax.sql.DataSource
-
-sealed interface Error {
-    class IA
-    class IB
+// A sealed class with a string property
+sealed class Fruit(val x: String) {
+    // Two subclasses of sealed class defined within
+    class Apple : Fruit("Apple")
+    class Mango : Fruit("Mango")
 }
 
-sealed class IOError(): Error
+// A subclass defined outside the sealed class
+class Pomegranate : Fruit("Pomegranate")
 
-class FileReadError(val file: File): IOError()
-class DatabaseError(val source: DataSource): IOError()
+// A function to take in an object of type Fruit
+// And to display an appropriate message depending on the type of Fruit
+fun display(fruit: Fruit) {
+    when (fruit) {
+        is Fruit.Apple -> println("${fruit.x} is good for iron")
+        is Fruit.Mango -> println("${fruit.x} is delicious")
+        is Pomegranate -> println("${fruit.x} is good for vitamin d")
+    }
+}
 
-object RuntimeError : Error
+fun main() {
+    // Objects of different subclasses created
+    val obj = Fruit.Apple()
+    val obj1 = Fruit.Mango()
+    val obj2 = Pomegranate()
+
+    // Function called with different objects
+    display(obj)
+    display(obj1)
+    display(obj2)
+}
